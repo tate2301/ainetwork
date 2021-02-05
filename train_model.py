81dcd8dbde57844e0f37c1786256a9b04896f141
tate2301 <chris@techzim.co.zw>
2021-02-05 18:25:11 +0200

    Refactor entry

--- train_model.py
+++ train_model.py
@@ -7,19 +7,28 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 # 1: unreliable
 # 0: reliable
 
+def trainModel():
+    print("[+] Starting training")
 
-train = pd.read_csv('')
-train.info()
-train = train.fillna(' leeg')
-train['total'] = train['author'] + train['text']
+    train = pd.read_csv('train.csv')
+    train.info()
+    train = train.fillna(' leeg')
+    train['total'] = train['author'] + train['text']
 
-count_vectorizer = CountVectorizer(ngram_range=(1, 2))
-counts = count_vectorizer.fit_transform(train['total'].values)
+    count_vectorizer = CountVectorizer(ngram_range=(1, 2))
+    counts = count_vectorizer.fit_transform(train['total'].values)
 
-classifier = MultinomialNB()
-targets = train['label'].values
-classifier.fit(counts, targets)
+    classifier = MultinomialNB()
+    targets = train['label'].values
+    classifier.fit(counts, targets)
 
-filename = 'classifier.model'
-pickle.dump(classifier, open(filename, 'wb'))
-pickle.dump(count_vectorizer, open('vectors.weights', 'wb'))
+    filename = 'classifier.model'
+    pickle.dump(classifier, open(filename, 'wb'))
+    pickle.dump(count_vectorizer, open('vectors.weights', 'wb'))
+
+    print("[+] Training complete")
+
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    trainModel()
